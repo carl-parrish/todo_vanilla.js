0870e21e3a7acc7c22fae980ad63990548271b6c
Carl Parrish <carl.parrish@gmail.com>
2017-01-05 14:32:53 -0700

    First Shareable version

---
+++ script.js
@@ -0,0 +1,53 @@
+var todoList = {
+ todos: [],
+ displayAll: function() {
+   console.log('My Todos:');
+   if (this.todos.length){
+     this.todos.map(function(todo){
+       let flag = todo.completed ? "(X) ": '( ) ';
+       console.log(flag + todo.todoText);
+     });
+   }else{
+     console.log('Your todolist is empty!');
+   }
+  },
+  addTodo: function(textValue) {
+    this.todos.push({
+      todoText: textValue,
+      completed: false
+    });
+    this.displayAll();
+  },
+  changeTodo: function(index, newTextValue) {
+    this.todos[index].todoText = newTextValue;
+    this.displayAll();
+  },
+  deleteTodo: function(index) {
+    this.todos.splice(index, 1);
+    this.displayAll();
+  },
+  toggleCompleted: function(index) {
+    let current = this.todos[index];
+    current.completed = !(current.completed);
+    this.displayAll();
+  },
+  toggleAll: function(){
+    let todosArray = this.todos;
+    let completedTodos = todosArray.filter(current=> current.completed === true);
+    let bool = completedTodos.length !== todosArray.length;
+    
+    todosArray.map(current=> current.completed = bool);
+    this.displayAll();
+  }
+};
+
+const displayTodosButton = document.getElementById('btn_display');
+const toggleAllButton    = document.getElementById('btn_toggleAll');
+
+displayTodosButton.addEventListener('click', function(){
+  todoList.displayAll();
+});
+
+toggleAllButton.addEventListener('click', function(){
+  todoList.toggleAll();
+});
